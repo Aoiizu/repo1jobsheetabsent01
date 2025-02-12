08f881be3282cfdcc184d49da03c6753f51d0e59
Aoiizu <86702988+Aoiizu@users.noreply.github.com>
2025-02-12 18:34:33 +0900

    Update Loop1.java

--- p1/JS1/Loop1.java
+++ p1/JS1/Loop1.java
@@ -1,19 +1,31 @@
 import java.util.Scanner;
 
-public class Loop1 {
+public class Loops1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        
         System.out.print("Input NIM: ");
-        String n = sc.next();
-        String result = "";
-        for (int i = 0; i < n.length(); i++) {
+        String nim = scanner.nextLine();
+        
+        int n = Integer.parseInt(nim.substring(nim.length() - 2));
+        
+        if (n < 10) {
+            n += 10;
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            if (i == 6 || i == 10) {
+                continue;
+            }
+            
             if (i % 2 == 1) {
-                result += n.charAt(i) + "*";
+                System.out.print("* ");
             } else {
-                result += n.charAt(i);
+                System.out.print(i + " ");
             }
         }
-        System.out.println(result);
+        
+        scanner.close();
     }
 }
 
